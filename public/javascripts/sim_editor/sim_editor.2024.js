e86a1788fdc3a9d49150ff906a75a3995ae6423d
ale <aledeum.saf@gmail.com>
2026-03-17 20:53:24 -0300

    fixed max score

--- public/javascripts/sim_editor/sim_editor.2024.js
+++ public/javascripts/sim_editor/sim_editor.2024.js
@@ -3094,7 +3094,7 @@ app.controller('SimEditorController', ['$scope', '$uibModal', '$log', '$http','$
                     <td>${checkpointScore}</td>
                     <td>${exitBonus}</td>
                     <td>${(victimScore + checkpointScore + exitBonus)}</td>
-                    <td>${2*(victimScore + checkpointScore + exitBonus)}</td>
+                    <td>${2.2*(victimScore + checkpointScore + exitBonus)}</td>
                 </tbody>
             </table>
         `;
